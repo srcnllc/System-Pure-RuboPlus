3c2adc00c9bf2b6ca0920fa2916371c7bb59eb82
Sercan LÜLLECİ <srcn_llc_@hotmail.com>
2025-02-23 11:31:04 +0300

    project finish react

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,10 +1,10 @@
 import Container from '../../Tools/Container/Container';
 import styles from './Header.module.scss';
 import React, { useEffect, useState } from 'react';
-import navList from '../../../jsonData/headerNavList.json'
+import navList from '../../../jsonData/headerNavList.json';
 
-export default function Header({transparent,relativePage}) {
-  const [active, setActive] = useState("");
+export default function Header({ transparent, relativePage }) {
+  const [active, setActive] = useState('');
   const [menuActive, setMenuActive] = useState(false);
   useEffect(() => {
     setActive(window.location.pathname);
@@ -12,62 +12,76 @@ export default function Header({transparent,relativePage}) {
 
   return (
     <>
-    <header className={`${styles.header} ${transparent ? styles['header-transparent'] : ''} ${relativePage ? styles['header-relative'] : ''}`}>
-    <Container large mobileContainer>
+      <header
+        className={`${styles.header} ${transparent ? styles['header-transparent'] : ''} ${
+          relativePage ? styles['header-relative'] : ''
+        }`}
+      >
+        <Container large mobileContainer>
           <div className={`${styles.topbar}`}>
             <div className={styles.logo}>
-              <img src={'/logo.svg'} alt="logo" />
+              <a href="/">
+                <img src="/logo.svg" alt="logo" />
+              </a>{' '}
             </div>
-            <nav className='d-md'>
+            <nav className="d-md">
               <ul className={styles.navItems}>
                 {navList.map((item) => (
-                  <li key={item.slug} className={active === item.slug? styles.active : ''} onClick={() => setActive(item.slug)}>
+                  <li
+                    key={item.slug}
+                    className={active === item.slug ? styles.active : ''}
+                    onClick={() => setActive(item.slug)}
+                  >
                     <a href={item.slug}>{item.title}</a>
                   </li>
                 ))}
               </ul>
             </nav>
             <div className={`${styles.buttons} d-md`}>
-              <button className='btn grayButton'>GİRİŞ</button>
-              <button className='btn pinkButton'>BAŞVUR</button>
+              <button className="btn grayButton">GİRİŞ</button>
+              <button className="btn pinkButton">BAŞVUR</button>
             </div>
             <div
               onClick={() => setMenuActive(!menuActive)}
-              className={`${styles.hamburger} ${
-                menuActive ? styles.menuActive : ''
-              } d-sm`}>
+              className={`${styles.hamburger} ${menuActive ? styles.menuActive : ''} d-sm`}
+            >
               <span />
               <span />
               <span />
             </div>
-            </div>
+          </div>
         </Container>
       </header>
-      <div  className={`${styles.mobileNav} ${menuActive ? styles.mobileActive : ''} d-sm`}>
+      <div className={`${styles.mobileNav} ${menuActive ? styles.mobileActive : ''} d-sm`}>
         <div className={styles.logo}>
-            <img src={'/logo.svg'} alt="logo" />
-            <div
-              onClick={() => setMenuActive(!menuActive)}
-              className={`${styles.hamburger} ${
-                menuActive ? styles.menuActive : ''
-              }`}>
-              <span />
-              <span />
-              <span />
-            </div>
-            </div>
-              <ul className={styles.navItems}>
-                {navList.map((item) => (
-                  <li key={item.slug} className={active === item.slug? styles.active : ''} onClick={() => setActive(item.slug)}>
-                    <a href={item.slug}>{item.title}</a>
-                  </li>
-                ))}
-              </ul>
-              <div className={styles.buttons}>
-                <button className='btn grayButton'>GİRİŞ</button>
-                <button className='btn pinkButton'>BAŞVVUR</button>
-              </div>
-            </div>
+          <a href="/">
+            <img src="/logo.svg" alt="logo" />
+          </a>
+          <div
+            onClick={() => setMenuActive(!menuActive)}
+            className={`${styles.hamburger} ${menuActive ? styles.menuActive : ''}`}
+          >
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <ul className={styles.navItems}>
+          {navList.map((item) => (
+            <li
+              key={item.slug}
+              className={active === item.slug ? styles.active : ''}
+              onClick={() => setActive(item.slug)}
+            >
+              <a href={item.slug}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className={styles.buttons}>
+          <button className="btn grayButton">GİRİŞ</button>
+          <button className="btn pinkButton">BAŞVVUR</button>
+        </div>
+      </div>
     </>
   );
 }
